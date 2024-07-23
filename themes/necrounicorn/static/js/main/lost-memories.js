ad05d14698e698803b68e57630e6f0970b931ab2
Vsezlotov <vsezlodov@gmail.com>
2024-07-23 22:10:39 +0300

    uptd

---
+++ themes/necrounicorn/static/js/main/lost-memories.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const topDiv = document.querySelector('.topDIV');
+  const buttonX = document.querySelector('.button-x');
+  // Get value from localStorage when open the page
+  const lS = localStorage.getItem('first-visit');
+  // Add button 'click' event
+  buttonX.addEventListener('click', () => {
+    // Set value to the localStorage
+    localStorage.setItem('first-visit', false);
+    // hide DOM element
+    topDiv.style.display = 'none';
+  });
+  // This does not check on the first visit to the page
+  // If localStorage have value, hide DOM element
+  if (lS) topDiv.style.display = 'none';
+});
