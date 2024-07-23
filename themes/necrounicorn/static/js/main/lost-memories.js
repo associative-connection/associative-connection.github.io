8fdaf05c9732e76dbc63ce77a703709484851ca5
Vsezlotov <vsezlodov@gmail.com>
2024-07-23 23:30:08 +0300

    uptd

--- themes/necrounicorn/static/js/main/lost-memories.js
+++ themes/necrounicorn/static/js/main/lost-memories.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const topDiv = document.querySelector('.window_welcome');
-  const buttonX = document.querySelector('.button-x');
+  const lost_memories = document.querySelector('.lost_memories');
+  const done = document.querySelector('.done');
   // Get value from localStorage when open the page
   const lS = localStorage.getItem('first-visit');
   // Add button 'click' event
-  buttonX.addEventListener('click', () => {
+  done.addEventListener('click', () => {
     // Set value to the localStorage
     localStorage.setItem('first-visit', false);
     // hide DOM element
-    topDiv.style.display = 'none';
+    lost_memories.style.display = 'none';
   });
   // This does not check on the first visit to the page
   // If localStorage have value, hide DOM element
-  if (lS) topDiv.style.display = 'none';
+  if (lS) lost_memories.style.display = 'none';
 });
